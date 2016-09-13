171cf2901475fb9036eef25e2adf16e6cd34d415
bug2bug <S.Sobko@profitware.ru>
2016-09-13 22:34:50 +0300

    ansible - packages

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ANSIBLE_PULL=/usr/bin/ansible-pull
+
+[ -x $ANSIBLE_PULL ] || sudo apt-get install -y ansible
+[ -x /usr/bin/git ] || sudo apt-get install -y git
+
+if [ -x $ANSIBLE_PULL ]
+then
+    echo "localhost ansible_connection=local" > /tmp/local_node_inventory
+    $ANSIBLE_PULL --url=https://github.com/profitware/webdev-hosting.git -K -i /tmp/local_node_inventory
+else
+    echo "Cannot find or install Ansible. Exiting."
+fi
