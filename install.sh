224df6c533b9d7c5885b7e902bf562ba6abb32d6
bug2bug <S.Sobko@profitware.ru>
2016-09-13 22:39:13 +0300

    ansible - debian specific

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ ANSIBLE_PULL=/usr/bin/ansible-pull
 if [ -x $ANSIBLE_PULL ]
 then
     echo "localhost ansible_connection=local" > /tmp/local_node_inventory
-    $ANSIBLE_PULL --url=https://github.com/profitware/webdev-hosting.git -K -i /tmp/local_node_inventory
+    $ANSIBLE_PULL --url=https://github.com/profitware/webdev-hosting.git -i /tmp/local_node_inventory
 else
     echo "Cannot find or install Ansible. Exiting."
 fi
